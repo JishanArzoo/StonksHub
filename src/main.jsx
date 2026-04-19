f01f419305eb1e6fd418485f0f15c1923e16c265
Jishan Arzoo <150323168+JishanArzoo@users.noreply.github.com>
2026-04-19 20:50:29 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   //<StrictMode>
